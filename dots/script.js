1fa97f7321d84a2148a4d049e01008f4d2b91e06
Aishwary Gupta <aish042004@gmail.com>
2026-03-10 20:21:44 +0530

    Update script.js

--- dots/script.js
+++ dots/script.js
@@ -89,6 +89,20 @@ loader.load(
     },
 );
 
+function disposeModel(root) {
+        if (!root) return;
+        root.traverse((obj) => {
+          if (obj.isMesh) {
+            if (obj.geometry) obj.geometry.dispose();
+            if (obj.material) {
+              if (Array.isArray(obj.material))
+                obj.material.forEach((m) => m.dispose && m.dispose());
+              else obj.material.dispose && obj.material.dispose();
+            }
+          }
+        });
+      }
+
 /* Shaders */
 
 const normalShader = `

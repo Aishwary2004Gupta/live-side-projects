d9e61d619b6e482fd2182f4cdc4bc02984501cc0
Aishwary Gupta <aish042004@gmail.com>
2026-03-10 20:22:58 +0530

    Update script.js

--- dots/script.js
+++ dots/script.js
@@ -90,18 +90,18 @@ loader.load(
 );
 
 function disposeModel(root) {
-        if (!root) return;
-        root.traverse((obj) => {
-          if (obj.isMesh) {
+    if (!root) return;
+    root.traverse((obj) => {
+        if (obj.isMesh) {
             if (obj.geometry) obj.geometry.dispose();
             if (obj.material) {
-              if (Array.isArray(obj.material))
-                obj.material.forEach((m) => m.dispose && m.dispose());
-              else obj.material.dispose && obj.material.dispose();
+                if (Array.isArray(obj.material))
+                    obj.material.forEach((m) => m.dispose && m.dispose());
+                else obj.material.dispose && obj.material.dispose();
             }
-          }
-        });
-      }
+        }
+    });
+}
 
 /* Shaders */
 

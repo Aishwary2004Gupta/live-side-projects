5787594ac4075e3c51851c1f48cb784521029375
Aishwary Gupta <aish042004@gmail.com>
2026-04-27 20:40:45 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1201,7 +1201,7 @@ const MODELS = {
         position: { x: 0.6, y: -0.2, z: 0 },
     },
     try: {
-        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/walther_p88_gun.glb",
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/gun.glb",
         scale: 0.1,
         rotation: { x: 0, y: 0, z: 0 },
         position: { x: 0, y: -0.8, z: 0 },

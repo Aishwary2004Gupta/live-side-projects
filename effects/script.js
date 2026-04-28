2ea5e1ffc0126c3681b172d427f78bd558e30d05
Aishwary Gupta <aish042004@gmail.com>
2026-04-28 23:03:24 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1206,7 +1206,7 @@ const MODELS = {
         rotation: { x: -0.4, y: Math.PI, z: 0 },
         position: { x: 0, y: -0.5, z: 0 },
     },
-    butterfly: {
+    butterfly: { //make sure to pin this model before making the site live
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/blue_monarch_butterfly.glb",
         scale: 1,
         rotation: { x: -1.2, y: Math.PI, z: 0 },

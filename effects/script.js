23644ef640f15a19e28fc201873bdadcae165815
Aishwary Gupta <aish042004@gmail.com>
2026-04-28 22:49:37 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1200,6 +1200,12 @@ const MODELS = {
         rotation: { x: 0, y: Math.PI + 1.2, z: 0 },
         position: { x: 0.6, y: -0.2, z: 0 },
     },
+    gun: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/gun.glb",
+        scale: 0.023,
+        rotation: { x: -0.4, y: Math.PI, z: 0 },
+        position: { x: 0, y: -0.5, z: 0 },
+    },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/gun.glb",
         scale: 0.023,

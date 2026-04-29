a153130767717777e80c2e765c923e94b0971995
Aishwary Gupta <aish042004@gmail.com>
2026-04-29 22:44:18 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1213,7 +1213,7 @@ const MODELS = {
         position: { x: 0, y: -0.2, z: 0 },
     },
     t_rex: {
-        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/blue_monarch_butterfly.glb",
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/t-rex.glb",
         scale: 1,
         rotation: { x: -1.2, y: Math.PI, z: 0 },
         position: { x: 0, y: -0.2, z: 0 },

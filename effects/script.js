baa54f4532ef53a18c9a1d8b352726abed84294c
Aishwary Gupta <aish042004@gmail.com>
2026-05-01 01:25:14 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1220,9 +1220,9 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/soda_can.glb",
-        scale: 0.5,
+        scale: 0.1,
         rotation: { x: 0, y: Math.PI, z: 0 },
-        position: { x: 0, y: -1.2, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
     },
 };
 

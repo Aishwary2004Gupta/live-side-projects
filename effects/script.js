d7c9837a10790734128964363b06438ec0f00ba2
Aishwary Gupta <aish042004@gmail.com>
2026-05-01 14:59:06 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1227,7 +1227,7 @@ const MODELS = {
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/simple_cola_can.glb",
         scale: 0.6,
-        rotation: { x: 0, y: -0.6, z: 0 },
+        rotation: { x: 0, y: -0.3, z: 0 },
         position: { x: 0, y: 0, z: 0 },
     },
 };

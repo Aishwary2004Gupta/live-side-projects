342ba5ba1e79facf965b3c81373aca7fe1c5f758
Aishwary Gupta <aish042004@gmail.com>
2026-05-02 21:40:09 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1230,6 +1230,12 @@ const MODELS = {
         rotation: { x: -0.2, y: Math.PI - 10, z: 0 },
         position: { x: 0, y: 0, z: 0 },
     },
+    try: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/simple_cola_can.glb",
+        scale: 0.6,
+        rotation: { x: -0.2, y: Math.PI - 10, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
+    },
 };
 
 function disposeModel(root) {

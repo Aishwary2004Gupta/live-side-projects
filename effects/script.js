f9113702b33f979411f8bed3be3a2b02359ad410
Aishwary Gupta <aish042004@gmail.com>
2026-05-02 22:59:08 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1232,7 +1232,7 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/guitar.glb",
-        scale: 0.6,
+        scale: 1,
         rotation: { x: 0, y: 0, z: 0 },
         position: { x: 0, y: 0, z: 0 },
     },

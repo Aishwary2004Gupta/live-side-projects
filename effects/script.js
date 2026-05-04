ad31ac96cd6d58deace5205093d4c5b6b5934cee
Aishwary Gupta <aish042004@gmail.com>
2026-05-04 22:04:55 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1238,9 +1238,9 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/electric_guitar_lowpoly_model.glb",
-        scale: 1.7,
-        rotation: { x: -1.8, y: -0.5, z: -0.3 },
-        position: { x: 0.2, y: 0, z: 0 },
+        scale: 1,
+        rotation: { x: 0, y: 0, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
     },
 };
 

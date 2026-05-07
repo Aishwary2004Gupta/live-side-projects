8219f8fb8a5ba25b2dfcc246506bc9045c0bcbf6
Aishwary Gupta <aish042004@gmail.com>
2026-05-07 21:13:11 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -691,9 +691,9 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/binoculars.glb",
-        scale: 0.38,
+        scale: 0.5,
         rotation: { x: 0, y: 0, z: 0 },
-        position: { x: 0, y: -1.3, z: 0 },
+        position: { x: 0, y: -1.5, z: 0 },
     },
 };
 

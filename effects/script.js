7084681fe36bfe90382aa077d28df4849d2ba991
Aishwary Gupta <aish042004@gmail.com>
2026-05-07 23:17:15 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -695,6 +695,12 @@ const MODELS = {
         rotation: { x: -0.2, y: 2.3, z: 0 },
         position: { x: 0.3, y: -1.7, z: 0 },
     },
+    try: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/binoculars.glb",
+        scale: 0.5,
+        rotation: { x: -0.2, y: 2.3, z: 0 },
+        position: { x: 0.3, y: -1.7, z: 0 },
+    },
 };
 
 function disposeModel(root) {

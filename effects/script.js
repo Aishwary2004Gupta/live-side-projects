081b4a772c87fc02fec6220c743e51c84817d3b2
Aishwary Gupta <aish042004@gmail.com>
2026-05-08 22:32:47 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -698,7 +698,7 @@ const MODELS = {
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/glasses_.glb",
         scale: 0.003,
-        rotation: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -1, z: 0 },
     },
 };

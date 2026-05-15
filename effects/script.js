8a0d01354ce44376675d90c10d51989d63a4581f
Aishwary Gupta <aish042004@gmail.com>
2026-05-15 23:10:21 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -715,7 +715,7 @@ const MODELS = {
     },
     starbucks_disposable_cup: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/starbucks_disposable_cup.glb",
-        scale: 0.0025,
+        scale: 1,
         rotation: { x: 0, y: Math.PI + 0.7, z: 0 },
         position: { x: -0.2, y: -0.1, z: 0 },
     },
@@ -804,7 +804,7 @@ function loadModel(name) {
     );
 }
 
-loadModel("try");
+loadModel("starbucks_disposable_cup");
 
 /* ================= POST ================= */
 const composer = new EffectComposer(renderer);

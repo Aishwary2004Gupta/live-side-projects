023bcee66bfd8694dbb5f3c48e08205e5d905c97
Aishwary Gupta <aish042004@gmail.com>
2026-05-16 22:43:07 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -715,7 +715,7 @@ const MODELS = {
     },
     apple: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/red_apple_-_realistic_fruit_asset.glb",
-        scale: 1,
+        scale: 10,
         rotation: { x: 0, y: 0, z: 0 },
         position: { x: 0, y: 0, z: 0 },
     },

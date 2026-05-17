6ee76f6db9d143ac0ce20701c769e3f3f0de1e64
Aishwary Gupta <aish042004@gmail.com>
2026-05-17 12:25:59 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -721,8 +721,8 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/son_goku.glb",
-        scale: 0.8,
-        rotation: { x: 0, y: 0, z: 0 },
+        scale: 0.7,
+        rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -1.2, z: 0 },
     },
 };

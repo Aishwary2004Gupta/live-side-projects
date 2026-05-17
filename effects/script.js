74302415094618dbddfb7383c60d94111fbaf3af
Aishwary Gupta <aish042004@gmail.com>
2026-05-17 12:05:23 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -723,7 +723,7 @@ const MODELS = {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/goku.glb",
         scale: 4,
         rotation: { x: 0, y: 0, z: 0 },
-        position: { x: 0, y: -0.9, z: 0 },
+        position: { x: 0, y: -1.15, z: 0 },
     },
 };
 

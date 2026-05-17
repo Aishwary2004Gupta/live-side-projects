9c4c745e7d3791616da4c936168f86779a168764
Aishwary Gupta <aish042004@gmail.com>
2026-05-17 21:41:00 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -737,12 +737,6 @@ const MODELS = {
         rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -1.1, z: 0 },
     },
-    // try: {
-    //     url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/son_goku.glb",
-    //     scale: 0.7,
-    //     rotation: { x: 0, y: Math.PI, z: 0 },
-    //     position: { x: 0, y: -1.1, z: 0 },
-    // },
 };
 
 function disposeModel(root) {

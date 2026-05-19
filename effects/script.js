b78b02f30faad9cd6bca43aadd400f3b6f07f15b
Aishwary Gupta <aish042004@gmail.com>
2026-05-19 19:30:32 +0530

    added a new model BULBASAUR

--- effects/script.js
+++ effects/script.js
@@ -731,7 +731,7 @@ const MODELS = {
         rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -1.1, z: 0 },
     },
-    try: {
+    bulbasaur: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/bulbasaur.glb",
         scale: 0.4,
         rotation: { x: 0, y: Math.PI - 0.4, z: 0 },

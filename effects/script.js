49de854c40ff3b2975fd1ce2787d06962db689fa
Aishwary Gupta <aish042004@gmail.com>
2026-05-21 21:25:46 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -751,9 +751,9 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/porygon.glb",
-        scale: 0.003,
+        scale: 0.0045,
         rotation: { x: 0, y: Math.PI + 0.5, z: 0 },
-        position: { x: 0, y: 0.2, z: 0 },
+        position: { x: 0, y: 0.9, z: 0 },
     },
 };
 

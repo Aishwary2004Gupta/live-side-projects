81bf98b43a5d41f048712ea2af1fbc06a92bef8b
Aishwary Gupta <aish042004@gmail.com>
2026-05-25 20:35:15 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -757,7 +757,7 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/magikarp.glb",
-        scale: 0.3,
+        scale: 0.23,
         rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -0.1, z: 0 },
     },

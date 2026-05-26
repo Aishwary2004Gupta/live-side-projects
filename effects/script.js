f5b2c5b73f49345f16aca1f0293977fbc2fa6ff1
Aishwary Gupta <aish042004@gmail.com>
2026-05-26 19:05:30 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -771,7 +771,7 @@ const MODELS = {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/mew_pokemon.glb",
         scale: 0.1,
         rotation: { x: 0, y: Math.PI, z: 0 },
-        position: { x: 0, y: -0.9, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
     },
 };
 

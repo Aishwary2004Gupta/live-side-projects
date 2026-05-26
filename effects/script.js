2dc9b97bf67df5fab3a2b7779d687d80c726972d
Aishwary Gupta <aish042004@gmail.com>
2026-05-26 18:39:16 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -765,7 +765,7 @@ const MODELS = {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/lapras.glb",
         scale: 0.3,
         rotation: { x: 0, y: Math.PI + 0.8, z: 0 },
-        position: { x: 0, y: -0.9, z: 0 },
+        position: { x: 0, y: -2, z: 0 },
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/lapras.glb",

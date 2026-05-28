8657e0a552369458ccc20aecb225bc5c64eeb3bb
Aishwary Gupta <aish042004@gmail.com>
2026-05-28 18:42:09 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -792,8 +792,8 @@ const MODELS = {
         position: { x: 0, y: 0, z: 0 },
     },
     try: {
-        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/the_starry_night.glb",
-        scale: 0.28,
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/the_mona_lisa.glb",
+        scale: 0.16,
         rotation: { x: 0, y: 2.6, z: 0 },
         position: { x: 0, y: 0, z: 0 },
     },

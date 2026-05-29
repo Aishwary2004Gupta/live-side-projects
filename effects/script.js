925973d195c90c3c5565e20e4bcc88534d843212
Aishwary Gupta <aish042004@gmail.com>
2026-05-29 22:40:20 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -1067,7 +1067,7 @@ modelListItems.forEach((item) => {
     });
 });
 
-switchEffect("dots"); //change default effect here
+switchEffect("normal"); //change default effect here
 
 pixelInput.oninput = (e) => {
     const value = +e.target.value;

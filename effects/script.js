8a87c7db660bd7a5f748f461a8e913ea03d32eed
Aishwary Gupta <aish042004@gmail.com>
2026-05-31 21:52:49 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -802,6 +802,12 @@ const MODELS = {
         rotation: { x: 0, y: 2.6, z: 0 },
         position: { x: 0, y: 0, z: 0 },
     },
+    try: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/the_mona_lisa.glb",
+        scale: 0.16,
+        rotation: { x: 0, y: 2.6, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
+    },
 };
 
 function disposeModel(root) {

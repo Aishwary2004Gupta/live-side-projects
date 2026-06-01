9585e76071d803ce1937f8ae1b2079903a579f58
Aishwary Gupta <aish042004@gmail.com>
2026-06-01 22:13:57 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -811,7 +811,7 @@ const MODELS = {
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/money_bag.glb",
         scale: 0.01,
-        rotation: { x: 0, y: Math.PI - 1, z: 0 },
+        rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -1, z: 0 },
     },
 };

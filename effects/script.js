e08ef9c5026a1a9acbda68e9ba4a38139f0923ff
Aishwary Gupta <aish042004@gmail.com>
2026-06-02 20:33:30 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -814,6 +814,12 @@ const MODELS = {
         rotation: { x: 0, y: 0, z: 0 },
         position: { x: 0, y: -0.8, z: 0 },
     },
+    try: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/money_bag.glb",
+        scale: 0.009, 
+        rotation: { x: 0, y: 0, z: 0 },
+        position: { x: 0, y: -0.8, z: 0 },
+    },
 };
 
 function disposeModel(root) {

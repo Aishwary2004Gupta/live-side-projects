8b44f42880e1f9998a761ee4339f5b5b690d6ad1
Aishwary Gupta <aish042004@gmail.com>
2026-06-02 20:49:09 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -814,6 +814,12 @@ const MODELS = {
         rotation: { x: 0, y: 0, z: 0 },
         position: { x: 0, y: -0.8, z: 0 },
     },
+    canon_800d: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/canon_800d.glb",
+        scale: 17, 
+        rotation: { x: 0, y: Math.PI - 1.6, z: -0.07 },
+        position: { x: 0, y: 0, z: 0 },
+    },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/canon_800d.glb",
         scale: 17, 

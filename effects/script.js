0560f8eb161d8eb8dd6fc5b629122d1c7921eec1
Aishwary Gupta <aish042004@gmail.com>
2026-06-05 22:19:32 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -840,10 +840,10 @@ const MODELS = {
         position: { x: 0, y: -0.8, z: 0 },
     },
     try: {
-        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/bloxx.glb",
-        scale: 1,
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/cannonbolt.glb",
+        scale: 0.01,
         rotation: { x: 0, y: Math.PI, z: 0 },
-        position: { x: 0, y: 0, z: 0 },
+        position: { x: 0, y: -1, z: 0 },
     },
 };
 

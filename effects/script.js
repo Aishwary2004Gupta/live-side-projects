fe3e06aba010a474dc72b6eb0417a27839cb2098
Aishwary Gupta <aish042004@gmail.com>
2026-06-05 22:01:11 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -837,7 +837,7 @@ const MODELS = {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/prime.glb",
         scale: 6,
         rotation: { x: 0, y: Math.PI, z: 0 },
-        position: { x: 0, y: -1, z: 0 },
+        position: { x: 0, y: -0.8, z: 0 },
     },
 };
 

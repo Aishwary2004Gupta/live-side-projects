f349871b223319ecd3e96d7d655d8e4e79088da1
Aishwary Gupta <aish042004@gmail.com>
2026-06-05 22:26:33 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -845,6 +845,12 @@ const MODELS = {
         rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -0.8, z: 0 },
     },
+    diamondhead: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/diamondhead.glb",
+        scale: 1,
+        rotation: { x: 0, y: Math.PI, z: 0 },
+        position: { x: 0, y: -1.2, z: 0 },
+    },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/diamondhead.glb",
         scale: 1,

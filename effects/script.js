4af3c9bf01c5b35be466a3c1dd40714a01561c16
Aishwary Gupta <aish042004@gmail.com>
2026-06-05 22:11:51 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -840,10 +840,10 @@ const MODELS = {
         position: { x: 0, y: -0.8, z: 0 },
     },
     try: {
-        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/prime.glb",
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/bloxx.glb",
         scale: 6,
         rotation: { x: 0, y: Math.PI, z: 0 },
-        position: { x: 0, y: -0.8, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
     },
 };
 

bd9764600dfd6f223c5b8832e07979547a1ca8fc
Aishwary Gupta <aish042004@gmail.com>
2026-06-09 21:42:53 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -875,6 +875,12 @@ const MODELS = {
         rotation: { x: -1, y: Math.PI + 0.7, z: -0.4 },
         position: { x: 0, y: 0, z: 0 },
     },
+    try: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/omnitrix2.glb",
+        scale: 0.35,
+        rotation: { x: -1, y: Math.PI + 0.7, z: -0.4 },
+        position: { x: 0, y: 0, z: 0 },
+    },
 };
 
 function disposeModel(root) {

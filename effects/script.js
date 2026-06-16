7877c8a9166fc96f745b15febb35804238562433
Aishwary Gupta <aish042004@gmail.com>
2026-06-16 22:19:32 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -893,6 +893,12 @@ const MODELS = {
         rotation: { x: 0, y: Math.PI + 0.5, z: 0 },
         position: { x: 0, y: -1.2, z: 0 },
     },
+    try: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/donald_duck.glb",
+        scale: 1.1,
+        rotation: { x: 0, y: Math.PI + 0.5, z: 0 },
+        position: { x: 0, y: -1.2, z: 0 },
+    },
 };
 
 function disposeModel(root) {

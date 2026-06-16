080bb7c6e70fa90614a916268bc03600ac44175f
Aishwary Gupta <aish042004@gmail.com>
2026-06-16 22:05:14 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -883,9 +883,9 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/mickey_mouse.glb",
-        scale: 1,
+        scale: 0.8,
         rotation: { x: 0, y: Math.PI, z: 0 },
-        position: { x: 0, y: -1, z: 0 },
+        position: { x: 0, y: -1.2, z: 0 },
     },
 };
 

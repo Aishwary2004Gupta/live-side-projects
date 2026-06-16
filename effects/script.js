bf06bad45f22bf974cfd9cf06d285ef2f821c460
Aishwary Gupta <aish042004@gmail.com>
2026-06-16 22:18:41 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -889,9 +889,9 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/donald_duck.glb",
-        scale: 1,
+        scale: 1.1,
         rotation: { x: 0, y: Math.PI + 0.5, z: 0 },
-        position: { x: 0, y: -1.1, z: 0 },
+        position: { x: 0, y: -1.2, z: 0 },
     },
 };
 

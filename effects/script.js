98f4af2ab3bfac4545bda49f2beee46935ac016c
Aishwary Gupta <aish042004@gmail.com>
2026-06-24 22:51:05 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -913,7 +913,7 @@ const MODELS = {
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/mandalorian_helmet.glb",
         scale: 1,
-        rotation: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -1.5, z: 0 },
     },
 };

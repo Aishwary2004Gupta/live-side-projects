7db37cb29021337b15d0aadb0441932a78ae5b86
Aishwary Gupta <aish042004@gmail.com>
2026-07-14 18:07:39 +0530

    Update app.js

--- Pixel Particles/app.js
+++ Pixel Particles/app.js
@@ -72,7 +72,7 @@ function createPixelData() {
         console.error("Failed to load image. Check CORS policy.");
     };
 
-    image.src = "https://images.unsplash.com/photo-1739162663907-e71e988e2e91?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    image.src = "https://images.unsplash.com/photo-1506606401543-2e73709cebb4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 }
 
 function createParticles() {

61d68f5bc0b790550f3ea4ac1e5df1051440362c
Aishwary Gupta <aish042004@gmail.com>
2026-08-08 22:59:42 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -925,7 +925,7 @@ const MODELS = {
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/carpet.glb",
         scale: 0.2,
-        rotation: { x: -1, y: 0, z: 6 },
+        rotation: { x: -1.5, y: 0, z: 6 },
         position: { x: 0, y: 0, z: 0 },
     },
 };

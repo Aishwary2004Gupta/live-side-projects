d296a4b60004f165f8a331c2c02e5a2cf27fa0b3
Aishwary Gupta <aish042004@gmail.com>
2026-08-27 23:15:32 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -942,7 +942,7 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/ice_cream_cone.glb",
-        scale: 2,
+        scale: 1,
         rotation: { x: 0, y: 0, z: 0 },
         position: { x: 0, y: -1, z: 0 },
     },

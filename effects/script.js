432902ebed12afbed6dadce1ee8b7f25084db4d0
Aishwary Gupta <aish042004@gmail.com>
2026-08-29 12:19:58 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -949,7 +949,7 @@ const MODELS = {
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/croissant.glb",
         scale: 0.0045,
-        rotation: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -0.5, z: 0 },
     },
 };

cae8ad52898d594d9d5f5410e28a441cb6f96e56
Aishwary Gupta <aish042004@gmail.com>
2026-09-01 22:55:56 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -955,7 +955,7 @@ const MODELS = {
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/box-truck.glb",
         scale: 0.045,
-        rotation: { x: -1.6, y: 0, z: 0 },
+        rotation: { x: 0, y: 0, z: 0 },
         position: { x: 0, y: -0.2, z: 0 },
     },
 };

f947741310a3a2dcdf3af07f7b05223d0a93534e
Aishwary Gupta <aish042004@gmail.com>
2026-09-07 13:15:45 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -956,7 +956,7 @@ const MODELS = {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/box-truck.glb",
         scale: 0.07,
         rotation: { x: 0, y: Math.PI + 1, z: 0 },
-        position: { x: 0, y: -0.5, z: 0 },
+        position: { x: 0, y: -0.8, z: 0 },
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/octopus_plush.glb",

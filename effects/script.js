4cd0b9c8163767174ecc35c551f6c4680dc029f9
Aishwary Gupta <aish042004@gmail.com>
2026-09-07 13:16:00 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -960,8 +960,8 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/octopus_plush.glb",
-        scale: 0.07,
-        rotation: { x: 0, y: Math.PI + 1, z: 0 },
+        scale: 0.7,
+        rotation: { x: 0, y: Math.PI, z: 0 },
         position: { x: 0, y: -0.5, z: 0 },
     },
 };

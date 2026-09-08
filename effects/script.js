a7f82fd52be21b12e465b39ba4ace487a040ef86
Aishwary Gupta <aish042004@gmail.com>
2026-09-08 22:23:56 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -964,6 +964,12 @@ const MODELS = {
         rotation: { x: -0.1, y: 1.5, z: 0 },
         position: { x: 0, y: -0.7, z: 0 },
     },
+    try: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/low_poly_treehouse.glb",
+        scale: 1,
+        rotation: { x: -0.1, y: 1.5, z: 0 },
+        position: { x: 0, y: -0.7, z: 0 },
+    },
 };
 
 function disposeModel(root) {

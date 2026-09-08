b7ac999816a9972911a25ee34430d0f5aba2d824
Aishwary Gupta <aish042004@gmail.com>
2026-09-08 16:25:17 +0530

    Update script.js

--- interstellar/script.js
+++ interstellar/script.js
@@ -1,20 +1,15 @@
-/*********
- * made by Matthias Hurrle (@atzedent)
- */
-let editMode = false // set to false to hide the code editor on load
-let resolution = .5 // set 1 for full resolution or to .5 to start with half resolution on load
-let renderDelay = 1000 // delay in ms before rendering the shader after a change
+let editMode = false 
+let resolution = 1 // Full sharp native resolution (no blur)
+let renderDelay = 1000 
 let dpr = Math.max(1, resolution * window.devicePixelRatio)
 let frm, source, editor, renderer, pointers
-const shaderId = 'K3pQzzW'
+
 window.onload = init
 
 function resize() {
     const { innerWidth: width, innerHeight: height } = window
-
     canvas.width = width * dpr
     canvas.height = height * dpr
-
     if (renderer) {
         renderer.updateScale(dpr)
     }
@@ -34,28 +29,40 @@ function toggleResolution() {
     pointers.updateScale(dpr)
     resize()
 }
+
+let accumulatedTime = 0
+let lastTime = 0
+
 function loop(now) {
+    if (!lastTime) lastTime = now
+    const delta = (now - lastTime)
+    lastTime = now
+
+    const btnPause = document.getElementById('btnTogglePause')
+    if (!btnPause || !btnPause.checked) {
+        accumulatedTime += delta
+    }
+
     renderer.updateMouse(pointers.first)
     renderer.updatePointerCount(pointers.count)
     renderer.updatePointerCoords(pointers.coords)
     renderer.updateMove(pointers.move)
     renderer.updateZoom(pointers.zoomed)
     renderer.updateWheel(pointers.wheel)
-    renderer.render(now)
+    renderer.render(accumulatedTime)
     frm = requestAnimationFrame(loop)
 }
+
 function renderThis() {
     editor.clearError()
     const shaderSource = normalizeShaderSource(editor.text)
-
     const result = renderer.test(shaderSource)
-
     if (result) {
         editor.setError(result)
     } else {
         renderer.updateShader(shaderSource)
     }
-    cancelAnimationFrame(frm) // Always cancel the previous frame!
+    cancelAnimationFrame(frm)
     loop(0)
 }
 function normalizeShaderSource(shaderSource) {
@@ -77,7 +84,7 @@ function init() {
     btnToggleResolution.addEventListener('change', toggleResolution)
     btnReset.addEventListener('click', reset)
 
-    document.title = "Elevator Visual"
+    document.title = "Interstellar Library"
 
     renderer = new Renderer(canvas, dpr)
     pointers = new PointerHandler(canvas, dpr)
@@ -89,10 +96,6 @@ function init() {
     if (!editMode) {
         btnToggleView.checked = true
         toggleView()
-    }
-    if (resolution === .5) {
-        btnToggleResolution.checked = true
-        toggleResolution()
     }
     canvas.addEventListener('shader-error', e => editor.setError(e.detail))
 
@@ -112,6 +115,7 @@ function init() {
         }
     })
 }
+
 class Renderer {
     #vertexSrc = "#version 300 es\nprecision highp float;\nin vec4 position;\nvoid main(){gl_Position=position;}"
     #fragmtSrc = "#version 300 es\nprecision highp float;\nout vec4 O;\nuniform float time;\nuniform vec2 resolution;\nvoid main() {\n\tvec2 uv=gl_FragCoord.xy/resolution;\n\tO=vec4(uv,sin(time)*.5+.5,1);\n}"
@@ -137,24 +141,12 @@ class Renderer {
         this.setup()
         this.init()
     }
-    updateMove(deltas) {
-        this.mouseMove = deltas
-    }
-    updateZoom(zoom) {
-        this.zoom = zoom
-    }
-    updateWheel(wheel) {
-        this.wheel = wheel
-    }
-    updateMouse(coords) {
-        this.mouseCoords = coords
-    }
-    updatePointerCoords(coords) {
-        this.pointerCoords = coords
-    }
-    updatePointerCount(nbr) {
-        this.nbrOfPointers = nbr
-    }
+    updateMove(deltas) { this.mouseMove = deltas }
+    updateZoom(zoom) { this.zoom = zoom }
+    updateWheel(wheel) { this.wheel = wheel }
+    updateMouse(coords) { this.mouseCoords = coords }
+    updatePointerCoords(coords) { this.pointerCoords = coords }
+    updatePointerCount(nbr) { this.nbrOfPointers = nbr }
     updateScale(scale) {
         this.scale = scale
         this.gl.viewport(0, 0, this.canvas.width * scale, this.canvas.height * scale)
@@ -163,7 +155,6 @@ class Renderer {
         const gl = this.gl
         gl.shaderSource(shader, source)
         gl.compileShader(shader)
-
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
             console.error(gl.getShaderInfoLog(shader))
             this.canvas.dispatchEvent(new CustomEvent('shader-error', { detail: gl.getShaderInfoLog(shader) }))
@@ -175,7 +166,6 @@ class Renderer {
         const shader = gl.createShader(gl.FRAGMENT_SHADER)
         gl.shaderSource(shader, source)
         gl.compileShader(shader)
-
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
             result = gl.getShaderInfoLog(shader)
         }
@@ -207,19 +197,13 @@ class Renderer {
         gl.attachShader(this.program, this.vs)
         gl.attachShader(this.program, this.fs)
         gl.linkProgram(this.program)
-
-        if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
-            console.error(gl.getProgramInfoLog(this.program))
-        }
     }
     init() {
         const { gl, program } = this
         this.buffer = gl.createBuffer()
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer)
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.#vertices), gl.STATIC_DRAW)
-
         const position = gl.getAttribLocation(program, "position")
-
         gl.enableVertexAttribArray(position)
         gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0)
 
@@ -234,7 +218,7 @@ class Renderer {
         program.wheel = gl.getUniformLocation(program, "wheel")
         program.startRandom = gl.getUniformLocation(program, "startRandom")
     }
-    render(now = 0) {
+    render(timeMs = 0) {
         const { gl, program, buffer, canvas, mouseMove, mouseCoords, pointerCoords, nbrOfPointers, zoom, wheel, startRandom } = this
         const daytime = new Date()
         if (!program || gl.getProgramParameter(program, gl.DELETE_STATUS)) return
@@ -244,7 +228,7 @@ class Renderer {
         gl.useProgram(program)
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
         gl.uniform2f(program.resolution, canvas.width, canvas.height)
-        gl.uniform1f(program.time, now * 1e-3)
+        gl.uniform1f(program.time, timeMs * 1e-3)
         gl.uniform4f(program.daytime, daytime.getHours(), daytime.getMinutes(), daytime.getSeconds(), daytime.getMilliseconds())
         gl.uniform2f(program.move, ...mouseMove)
         gl.uniform2f(program.touch, ...mouseCoords)
@@ -255,11 +239,8 @@ class Renderer {
         gl.uniform1f(program.startRandom, startRandom)
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
     }
-    clear() {
-        const { gl } = this
-        gl.clear(gl.COLOR_BUFFER_BIT)
-    }
 }
+
 class PointerHandler {
     constructor(element, scale) {
         this.scale = scale
@@ -280,16 +261,12 @@ class PointerHandler {
             this.ey = e.clientY
         })
         element.addEventListener("pointerup", (e) => {
-            if (this.count === 1) {
-                this.lastCoords = this.first
-            }
+            if (this.count === 1) this.lastCoords = this.first
             this.pointers.delete(e.pointerId)
             this.active = this.pointers.size > 0
         })
         element.addEventListener("pointerleave", (e) => {
-            if (this.count === 1) {
-                this.lastCoords = this.first
-            }
+            if (this.count === 1) this.lastCoords = this.first
             this.pointers.delete(e.pointerId)
             this.active = this.pointers.size > 0
         })
@@ -303,7 +280,6 @@ class PointerHandler {
         })
         element.addEventListener("wheel", (e) => {
             this.zoom = lerp(this.zoom, Math.max(-1, Math.min(1, this.zoom + e.deltaY)), .05)
-            // Bei Richtungswechsel sofort anpassen, sonst interpolieren
             if (this.wheelDelta * e.deltaY < 0) {
                 this.wheelDelta = e.deltaY
             } else {
@@ -312,40 +288,17 @@ class PointerHandler {
             this.wheelOffset += this.wheelDelta
         }, { passive: true })
     }
-    getScale() {
-        return this.scale
-    }
+    getScale() { return this.scale }
     updateScale(scale) { this.scale = scale }
-    reset() {
-        this.pointers.clear()
-        this.active = false
-        this.moves = [0, 0]
-        this.zoom = 0
-        this.wheelDelta = 0
-        this.wheelOffset = 0
-    }
-    get count() {
-        return this.pointers.size
-    }
-    get move() {
-        return this.moves
-    }
-    get zoomed() {
-        return this.zoom
-    }
-    get wheel() {
-        return [this.wheelDelta, this.wheelOffset] || [0, 0]
-    }
-    get coords() {
-        return this.pointers.size > 0 ? Array.from(this.pointers.values()).map((p) => [...p]).flat() : [0, 0]
-    }
-    get first() {
-        return this.pointers.values().next().value || this.lastCoords
-    }
+    get count() { return this.pointers.size }
+    get move() { return this.moves }
+    get zoomed() { return this.zoom }
+    get wheel() { return [this.wheelDelta, this.wheelOffset] || [0, 0] }
+    get coords() { return this.pointers.size > 0 ? Array.from(this.pointers.values()).map((p) => [...p]).flat() : [0, 0] }
+    get first() { return this.pointers.values().next().value || this.lastCoords }
 }
-function lerp(a, b, t) {
-    return a + (b - a) * t
-}
+function lerp(a, b, t) { return a + (b - a) * t }
+
 class Editor {
     constructor(textarea, errorfield, errorindicator) {
         this.textarea = textarea
@@ -358,25 +311,18 @@ class Editor {
     set hidden(value) { value ? this.#hide() : this.#show() }
     get text() { return this.textarea.value }
     set text(value) { this.textarea.value = value }
-    get scrollTop() { return this.textarea.scrollTop }
-    set scrollTop(value) { this.textarea.scrollTop = value }
     setError(message) {
         this.errorfield.innerHTML = message
         this.errorfield.classList.add('opaque')
         const match = message.match(/ERROR: \d+:(\d+):/)
         const lineNumber = match ? parseInt(match[1]) : 0
         const overlay = document.createElement('pre')
-
         overlay.classList.add('overlay')
         overlay.textContent = '\n'.repeat(lineNumber)
-
         document.body.appendChild(overlay)
-
         const offsetTop = parseInt(getComputedStyle(overlay).height)
-
         this.errorindicator.style.setProperty('--top', offsetTop + 'px')
         this.errorindicator.style.visibility = 'visible'
-
         document.body.removeChild(overlay)
     }
     clearError() {
@@ -385,23 +331,15 @@ class Editor {
         this.errorfield.blur()
         this.errorindicator.style.visibility = 'hidden'
     }
-    focus() {
-        this.textarea.focus()
-    }
+    focus() { this.textarea.focus() }
     #hide() {
-        for (const el of [this.errorindicator, this.errorfield, this.textarea]) {
-            el.classList.add('hidden')
-        }
+        for (const el of [this.errorindicator, this.errorfield, this.textarea]) el.classList.add('hidden')
     }
     #show() {
-        for (const el of [this.errorindicator, this.errorfield, this.textarea]) {
-            el.classList.remove('hidden')
-        }
+        for (const el of [this.errorindicator, this.errorfield, this.textarea]) el.classList.remove('hidden')
         this.focus()
     }
-    handleScroll() {
-        this.errorindicator.style.setProperty('--scroll-top', `${this.textarea.scrollTop}px`)
-    }
+    handleScroll() { this.errorindicator.style.setProperty('--scroll-top', `${this.textarea.scrollTop}px`) }
     handleKeydown(event) {
         if (event.key === "Tab") {
             event.preventDefault()
@@ -413,10 +351,7 @@ class Editor {
     }
     handleTabKey(shiftPressed) {
         if (this.#getSelectedText() !== "") {
-            if (shiftPressed) {
-                this.#unindentSelectedText()
-                return
-            }
+            if (shiftPressed) { this.#unindentSelectedText(); return; }
             this.#indentSelectedText()
         } else {
             this.#indentAtCursor()
@@ -424,34 +359,25 @@ class Editor {
     }
     #getSelectedText() {
         const editor = this.textarea
-        const start = editor.selectionStart
-        const end = editor.selectionEnd
-        return editor.value.substring(start, end)
+        return editor.value.substring(editor.selectionStart, editor.selectionEnd)
     }
     #indentAtCursor() {
         const editor = this.textarea
         const cursorPos = editor.selectionStart
-
         document.execCommand('insertText', false, '\t')
         editor.selectionStart = editor.selectionEnd = cursorPos + 1
     }
     #indentSelectedText() {
         const editor = this.textarea
         const cursorPos = editor.selectionStart
-        const selectedText = this.#getSelectedText()
-        const lines = selectedText.split('\n')
-        const indentedText = lines.map(line => '\t' + line).join('\n')
-
+        const indentedText = this.#getSelectedText().split('\n').map(line => '\t' + line).join('\n')
         document.execCommand('insertText', false, indentedText)
         editor.selectionStart = cursorPos
     }
     #unindentSelectedText() {
         const editor = this.textarea
         const cursorPos = editor.selectionStart
-        const selectedText = this.#getSelectedText()
-        const lines = selectedText.split('\n')
-        const indentedText = lines.map(line => line.replace(/^\t/, '').replace(/^ /, '')).join('\n')
-
+        const indentedText = this.#getSelectedText().split('\n').map(line => line.replace(/^\t/, '').replace(/^ /, '')).join('\n')
         document.execCommand('insertText', false, indentedText)
         editor.selectionStart = cursorPos
     }
@@ -459,31 +385,15 @@ class Editor {
         const editor = this.textarea
         const visibleTop = editor.scrollTop
         const cursorPosition = editor.selectionStart
-
         let start = cursorPosition - 1
-        while (start >= 0 && editor.value[start] !== '\n') {
-            start--
-        }
-
+        while (start >= 0 && editor.value[start] !== '\n') start--
         let newLine = ''
         while (start < cursorPosition - 1 && (editor.value[start + 1] === ' ' || editor.value[start + 1] === '\t')) {
             newLine += editor.value[start + 1]
             start++
         }
-
         document.execCommand('insertText', false, '\n' + newLine)
         editor.selectionStart = editor.selectionEnd = cursorPosition + 1 + newLine.length
-        editor.scrollTop = visibleTop // Prevent the editor from scrolling
-        const lineHeight = editor.scrollHeight / editor.value.split('\n').length
-        const line = editor.value.substring(0, cursorPosition).split('\n').length
-
-        // Do the actual layout calculation in order to get the correct scroll position
-        const visibleBottom = editor.scrollTop + editor.clientHeight
-        const lineTop = lineHeight * (line - 1)
-        const lineBottom = lineHeight * (line + 2)
-
-        // If the cursor is outside the visible range, scroll the editor
-        if (lineTop < visibleTop) editor.scrollTop = lineTop
-        if (lineBottom > visibleBottom) editor.scrollTop = lineBottom - editor.clientHeight
+        editor.scrollTop = visibleTop
     }
 }

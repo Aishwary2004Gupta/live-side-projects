8de9930436bcfd2120a9a44b939b0bac3d4a3ab1
Aishwary Gupta <aish042004@gmail.com>
2026-09-12 22:47:05 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -972,9 +972,9 @@ const MODELS = {
     },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/minecraft_turtle.glb",
-        scale: 0.06,
-        rotation: { x: -0.1, y: 1.5, z: 0 },
-        position: { x: 0, y: -0.7, z: 0 },
+        scale: 0.0006,
+        rotation: { x: 0, y: Math.PI, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
     },
 };
 

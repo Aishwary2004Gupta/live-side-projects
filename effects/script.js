ff0d43f9b1c3219c8a703f254888158f1f791d0d
Aishwary Gupta <aish042004@gmail.com>
2026-09-17 12:25:58 +0530

    Update script.js

--- effects/script.js
+++ effects/script.js
@@ -974,6 +974,12 @@ const MODELS = {
         rotation: { x: -0.1, y: 1.5, z: 0 },
         position: { x: 0, y: -0.7, z: 0 },
     },
+    sea_turtle: {
+        url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/sea_turtle1.glb",
+        scale: 0.05,
+        rotation: { x: 0, y: 1, z: 0 },
+        position: { x: 0, y: 0, z: 0 },
+    },
     try: {
         url: "https://raw.githubusercontent.com/Aishwary2004Gupta/models/cloud/sea_turtle1.glb",
         scale: 0.05,
